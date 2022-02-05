210aff0435e9ece2a8bdb9b11c18d68881fb85c6
Vitaliy901 <allanamos90@gmail.com>
2022-02-05 13:51:07 +0200

    add elemens to JS.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let display = document.querySelector('.view');
+let operators = document.querySelectorAll('.operators');
+let numbers = document.querySelectorAll('.nb');
+let clean = document.querySelector('clean');
+let clear = document.querySelector('.clear');
+let equal = document.querySelector('.equals');
