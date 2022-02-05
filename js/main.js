ca9e34b11d5faf0369d4ac353898a5b316c4a564
Vitaliy901 <allanamos90@gmail.com>
2022-02-05 15:33:16 +0200

    add functions for numbers and clean, clear.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 let display = document.querySelector('.view');
 let operators = document.querySelectorAll('.operators');
 let numbers = document.querySelectorAll('.nb');
-let clean = document.querySelector('clean');
+let clean = document.querySelector('.clean');
 let clear = document.querySelector('.clear');
 let equal = document.querySelector('.equals');
+
+
+for (let number of numbers) {
+    number.addEventListener('click', function () {
+        display.innerHTML += this.innerHTML
+    })
+}
+clean.addEventListener('click', function () {
+    let arr = display.innerHTML.split('');
+    arr.pop()
+    display.innerHTML = arr.join('');
+});
+clear.addEventListener('click', function () {
+    display.innerHTML = '';
+});
