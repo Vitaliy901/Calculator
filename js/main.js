8d177f5d1e1db555b1204c6f7ef1618abf64ccf6
Vitaliy901 <allanamos90@gmail.com>
2022-02-06 09:07:48 +0200

    fix classes and js.

--- js/main.js
+++ js/main.js
@@ -2,23 +2,21 @@ let allelements = document.querySelectorAll('button');
 let display = document.querySelector('.display');
 let view = document.querySelector('.view');
 let operators = document.querySelectorAll('.operator');
-let numbers = document.querySelectorAll('.nb');
 let clean = document.querySelector('.clean');
 let clear = document.querySelector('.clear');
 let equal = document.querySelector('.equals');
+
 // =====================displaySize=========================
 
-const viewWidth = view.offsetWidth;
+const viewWidth = view.offsetWidth - 10;
 function displaySize() {
     display.classList.remove('newSize');
     if (display.offsetWidth >= viewWidth) {
         display.classList.add('newSize');
     }
-    
-
 }
 
-// изьятие ","
+// получение "," из масива 
 let dot;
 for (let elem of operators) {
     if (elem.innerHTML == ',') {
@@ -26,7 +24,7 @@ for (let elem of operators) {
     }
 }
 
-// всем кнопкам кроме "," clean, clear, equals повесить событие.
+// всем кнопкам кроме "," clean, clear, equals вешает событие.
 for (let elem of allelements) {
     if (elem.className !== "number clean" && elem.className !== "number clear" 
     && elem.className!== "equals" && elem.innerHTML !== ",") {
@@ -37,34 +35,36 @@ for (let elem of allelements) {
     }
 }
 
-
-
-
-
-
-
-
-
-
 dot.addEventListener('click', function () {
-    if (display.innerHTML == '' && dot.innerHTML !== display.innerHTML) {
-        display.innerHTML = dot.innerHTML
+    if (display.innerHTML == '' && '.' !== display.innerHTML) {
+        display.innerHTML = '.'
         displaySize()
     }
     if (Number.isInteger(+display.innerHTML )) {
-        display.innerHTML += dot.innerHTML
+        display.innerHTML += '.'
         displaySize()
     }
 });
-
-
+// стереть символ.
 clean.addEventListener('click', function () {
     let arr = display.innerHTML.split('');
     arr.pop()
     display.innerHTML = arr.join('');
     displaySize()
 });
+// очистить поле.
 clear.addEventListener('click', function () {
     display.innerHTML = '';
     displaySize()
 });
+
+//========================equal=======================
+equal.addEventListener('click', calc);
+
+
+//==============calc=====================
+function calc(elem) {
+    if (condition) {
+        
+    }
+}
